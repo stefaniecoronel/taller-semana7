3fb7c3e3dfdf414bccf54cc67ee30075be185688
Sofia Olivera <sogon2009@gmail.com>
2024-08-15 10:44:14 -0300

    validacionkk

--- js/validacion.js
+++ js/validacion.js
@@ -1,41 +1,28 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
 
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-}
-
-<<<<<<< Updated upstream
-
-let contraseña = document.getElementById("password1");
-let repetirContraseña = document.getElementById("password2");
-let registro = document.getElementsById("regBtn");
-
-registro.addEventListener("submit", function(verificación) {
-    if (contraseña.value !== repetirContraseña.value) {
-        alert("Las contraseñas no coinciden.");
-        verificación.preventDefault();
+ 
+    function showAlertSuccess() {
+        document.getElementById("alert-success").classList.add("show");
     }
-});
-=======
-function requisitosRegistro() {
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const email = document.getElementById('email').value;
-    const password1 = document.getElementById('password1').value;
-    const password2 = document.getElementById('password2').value;
-    const terminos = document.getElementById('terminos').checked;
     
-}
-
-if (nombre && apellido && email && password1 && password2 && terminos) {
-    showAlertSuccess();
-}else { showAlertError();
-
+    function showAlertError() {
+        document.getElementById("alert-danger").classList.add("show");
     }
+    
+    
+        document.getElementById('regBtn').addEventListener('click', function(){
+            
+            const nombre = document.getElementById('nombre').value;
+            const apellido = document.getElementById('apellido').value;
+            const email = document.getElementById('email').value;
+            const password1 = document.getElementById('password1').value;
+            const password2 = document.getElementById('password2').value;
+            const terminos = document.getElementById('terminos').checked;
+            if (nombre && apellido && email && terminos && password1 === password2) {
+        
+                showAlertSuccess();
+            }else { showAlertError();
+            
+                }
+        });
 
 
-
-document.getElementById('regBtn').addEventListener('click', requisitosRegistro);
->>>>>>> Stashed changes
