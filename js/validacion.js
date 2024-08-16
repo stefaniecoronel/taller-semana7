e3ffa134d33242b1cc784efb598f1501c0d50e4e
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-08-16 12:17:44 -0300

    Nuevas condiciones para la función
    Agregué condiciones que buscan contemplar que los campos de contraseña se completen y a su vez cumpla con la longitud mínima.

--- js/validacion.js
+++ js/validacion.js
@@ -16,8 +16,10 @@
             const email = document.getElementById('email').value;
             const password1 = document.getElementById('password1').value;
             const password2 = document.getElementById('password2').value;
+            const password1Largo = password1.length; // password1 es un string y quiero obtener su largo para compararlo con el atributo minlength
+            const password1Minimo = document.getElementById('password1').getAttribute('minlength'); // acá colo en una variable el atributo minlength
             const terminos = document.getElementById('terminos').checked;
-            if (nombre && apellido && email && terminos && password1 === password2) {
+            if (nombre && apellido && email && terminos && password1 && password2 && password1 === password2 && password1Largo >= password1Minimo) {
         
                 showAlertSuccess();
             }else { showAlertError();
